26743e0870900ee9077744fda1271aeba712c34e
Marcio Carolino <marciocarolino@gmail.com>
2024-09-05 20:51:39 -0300

    accounts_payable

--- back/src/app/accounts_payable/accountsPayable.controller.ts
+++ back/src/app/accounts_payable/accountsPayable.controller.ts
@@ -18,32 +18,27 @@ export class AccountsPayableController {
     private readonly accountsPayableService: AccountsPayableService,
   ) {}
 
-  // Create - POST /accounts-payable
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() createAccountsPayableDto: CreateAccounutsPayableDTO) {
     return this.accountsPayableService.create(createAccountsPayableDto);
   }
 
-  // Read All - GET /accounts-payable
   @Get()
   findAll() {
     return this.accountsPayableService.findAll();
   }
 
-  // Read One by ID - GET /accounts-payable/:id
   @Get(":id")
   findOne(@Param("id") id: number) {
     return this.accountsPayableService.findOne(id);
   }
 
-  // Update - PATCH /accounts-payable/:id
   @Patch(":id")
   update(@Param("id") id: number, @Body() updateAccountsPayableDto: any) {
     return this.accountsPayableService.update(id, updateAccountsPayableDto);
   }
 
-  // Delete - DELETE /accounts-payable/:id
   @Delete(":id")
   @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param("id") id: number) {
